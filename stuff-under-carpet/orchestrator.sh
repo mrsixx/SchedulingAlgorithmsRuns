b2bb3b1e0eaafe5995194e24feba7195c3c10088
Matheus Ribeiro <matheus1999201000@gmail.com>
2025-08-05 01:19:11 +0000

    cleaning home

---
+++ stuff-under-carpet/orchestrator.sh
@@ -0,0 +1,67 @@
+#find folder -name '*.json' -exec mv {} to_path \;
+#find folder -name '*V0*.json' -exec mv {} to_path \;
+#find folder -regextype posix-extended -regex '.*/HurinkSdata([4-9]|[1-3][0-9]|4[0-3])\.fjs\..*V2.*\.csv' -exec mv {} to_path \;
+
+echo "iniciando compilação dos resultados"
+
+#python3 result-compiler.py results_dir output_prefix renamer_fn
+
+# python3 result-compiler.py ant-battle-royale/070525/6_Fattahi/V0 6_Fattahi-V0 fattahi
+# python3 result-compiler.py ant-battle-royale/070525/6_Fattahi/V1 6_Fattahi-V1 fattahi
+# python3 result-compiler.py ant-battle-royale/070525/6_Fattahi/V2 6_Fattahi-V2 fattahi
+# python3 result-compiler.py ant-battle-royale/070525/6_Fattahi/V3 6_Fattahi-V3 fattahi
+
+
+# python3 result-compiler.py ant-battle-royale/090525/1_Brandimarte/V0 1_Brandimarte-V0 brandimarte
+# python3 result-compiler.py ant-battle-royale/090525/1_Brandimarte/V1 1_Brandimarte-V1 brandimarte
+# python3 result-compiler.py ant-battle-royale/090525/1_Brandimarte/V2 1_Brandimarte-V2 brandimarte
+# python3 result-compiler.py ant-battle-royale/090525/1_Brandimarte/V3 1_Brandimarte-V3 brandimarte
+
+
+# python3 result-compiler.py ant-battle-royale/090525/9_Ribeiro/V2 9_Ribeiro-V2 ribeiro
+# python3 result-compiler.py ant-battle-royale/090525/9_Ribeiro/V3 9_Ribeiro-V3 ribeiro
+
+# python3 result-compiler.py ant-battle-royale/090525/3_DPpaulli/V2 3_DPpaulli-V2 paulli
+# python3 result-compiler.py ant-battle-royale/090525/3_DPpaulli/V3 3_DPpaulli-V3 paulli
+
+# python3 result-compiler.py ant-battle-royale/30052025/6_Fattahi/V0 6_Fattahi-V0 fattahi
+# python3 result-compiler.py ant-battle-royale/30052025/6_Fattahi/V1 6_Fattahi-V1 fattahi
+
+
+# python3 result-compiler.py ant-battle-royale/30052025/2a_Hurink_sdata/V2/Lawrence 2a_Hurink_sdata-V2 lawrence
+# python3 result-compiler.py ant-battle-royale/30052025/2a_Hurink_sdata/V3/Lawrence 2a_Hurink_sdata-V3 lawrence
+# python3 result-compiler.py ant-battle-royale/30052025/2d_Hurink_vdata/V2/Lawrence 2d_Hurink_vdata-V2 lawrence
+# python3 result-compiler.py ant-battle-royale/30052025/2d_Hurink_vdata/V3/Lawrence 2d_Hurink_vdata-V3 lawrence
+
+#python3 result-compiler.py ant-battle-royale/30052025/5_Kacem/V2 5_Kacem-V2 kacem
+#python3 result-compiler.py ant-battle-royale/30052025/5_Kacem/V3 5_Kacem-V3 kacem
+
+#python3 result-compiler.py RibeiroSuzarte/1-v2 RibeiroSuzarte1-V2 ribeiro
+#python3 result-compiler.py RibeiroSuzarte/2-v2 RibeiroSuzarte2-V2 ribeiro
+#python3 result-compiler.py RibeiroSuzarte/3-v2 RibeiroSuzarte3-V2 ribeiro
+#python3 result-compiler.py RibeiroSuzarte/4-v2 RibeiroSuzarte4-V2 ribeiro
+
+
+#python3 result-compiler.py RibeiroSuzarte/1-v3 RibeiroSuzarte1-V3 ribeiro
+#python3 result-compiler.py RibeiroSuzarte/2-v3 RibeiroSuzarte2-V3 ribeiro
+#python3 result-compiler.py RibeiroSuzarte/3-v3 RibeiroSuzarte3-V3 ribeiro
+
+#local searh
+#python3 result-compiler.py local_search/6_Fattahi/ 6_Fattahi-V2ls fattahi
+#python3 result-compiler.py new-tests/240625/2a_Hurink_sdata/ls/lawrence/ 2a_Hurink_sdata-V2ls lawrence
+python3 result-compiler.py new-tests/240625/2a_Hurink_sdata/llm/ 2a_Hurink_sdata-V2llm lawrence
+python3 result-compiler.py new-tests/240625/2a_Hurink_sdata/llm-ls/ 2a_Hurink_sdata-V2llmls lawrence
+
+python3 result-compiler.py new-tests/240625/6_Fattahi/llm/ 6_Fattahi-V2llm fattahi
+python3 result-compiler.py new-tests/240625/6_Fattahi/llm-ls/ 6_Fattahi-V2llmls fattahi
+
+python3 result-compiler.py new-tests/240625/1_Brandimarte/llm/ 1_Brandimarte-V2llm brandimarte
+python3 result-compiler.py new-tests/240625/1_Brandimarte/llm-ls/ 1_Brandimarte-V2llmls brandimarte
+
+python3 result-compiler.py new-tests/240625/2d_Hurink_vdata/llm/ 2d_Hurink_vdata-V2llm lawrence
+python3 result-compiler.py new-tests/240625/2d_Hurink_vdata/llm-ls/ 2d_Hurink_vdata-V2llmls lawrence
+
+python3 result-compiler.py new-tests/240625/3_DPpaulli/llm/ 3_DPpaulli-V2llm paulli
+python3 result-compiler.py new-tests/240625/3_DPpaulli/llm-ls/ 3_DPpaulli-V2llmls paulli
+
+echo "compilação finalizada"
